76f4237e9c3f8acd3737e54d6fe83e6310e4e6f3
nalaxyzz <milcentenzo@gmail.com>
2022-05-29 21:16:22 +0200

    Version 1.2.1

---
+++ src/main/java/net/mcreator/hydrexium/entity/EntitySouris.java
@@ -0,0 +1,131 @@
+
+package net.mcreator.hydrexium.entity;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.EntityRegistry;
+import net.minecraftforge.fml.common.registry.EntityEntryBuilder;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
+
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.World;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.DamageSource;
+import net.minecraft.item.Item;
+import net.minecraft.entity.ai.EntityAIWander;
+import net.minecraft.entity.ai.EntityAISwimming;
+import net.minecraft.entity.ai.EntityAIPanic;
+import net.minecraft.entity.ai.EntityAILookIdle;
+import net.minecraft.entity.ai.EntityAIHurtByTarget;
+import net.minecraft.entity.ai.EntityAIAttackMelee;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.EnumCreatureType;
+import net.minecraft.entity.EnumCreatureAttribute;
+import net.minecraft.entity.EntityCreature;
+import net.minecraft.entity.Entity;
+import net.minecraft.client.renderer.entity.RenderLiving;
+import net.minecraft.client.model.ModelSilverfish;
+
+import net.mcreator.hydrexium.ElementsHydrexiumMod;
+
+@ElementsHydrexiumMod.ModElement.Tag
+public class EntitySouris extends ElementsHydrexiumMod.ModElement {
+	public static final int ENTITYID = 2;
+	public static final int ENTITYID_RANGED = 3;
+	public EntitySouris(ElementsHydrexiumMod instance) {
+		super(instance, 123);
+	}
+
+	@Override
+	public void initElements() {
+		elements.entities.add(() -> EntityEntryBuilder.create().entity(EntityCustom.class).id(new ResourceLocation("hydrexium", "souris"), ENTITYID)
+				.name("souris").tracker(64, 3, true).egg(-10066330, -39169).build());
+	}
+
+	@Override
+	public void init(FMLInitializationEvent event) {
+		Biome[] spawnBiomes = {Biome.REGISTRY.getObject(new ResourceLocation("plains")), Biome.REGISTRY.getObject(new ResourceLocation("forest")),
+				Biome.REGISTRY.getObject(new ResourceLocation("taiga")),};
+		EntityRegistry.addSpawn(EntityCustom.class, 10, 4, 4, EnumCreatureType.MONSTER, spawnBiomes);
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void preInit(FMLPreInitializationEvent event) {
+		RenderingRegistry.registerEntityRenderingHandler(EntityCustom.class, renderManager -> {
+			RenderLiving customRender = new RenderLiving(renderManager, new ModelSilverfish(), 0.5f) {
+				protected ResourceLocation getEntityTexture(Entity entity) {
+					return new ResourceLocation("hydrexium:textures/souris.png");
+				}
+			};
+			customRender.addLayer(new net.minecraft.client.renderer.entity.layers.LayerHeldItem(customRender));
+			return customRender;
+		});
+	}
+	public static class EntityCustom extends EntityCreature {
+		public EntityCustom(World world) {
+			super(world);
+			setSize(0.4f, 0.3f);
+			experienceValue = 0;
+			this.isImmuneToFire = false;
+			setNoAI(!true);
+		}
+
+		@Override
+		protected void initEntityAI() {
+			super.initEntityAI();
+			this.tasks.addTask(1, new EntityAIAttackMelee(this, 1.2, false));
+			this.tasks.addTask(2, new EntityAIWander(this, 1));
+			this.targetTasks.addTask(3, new EntityAIHurtByTarget(this, false));
+			this.tasks.addTask(4, new EntityAILookIdle(this));
+			this.tasks.addTask(5, new EntityAISwimming(this));
+			this.tasks.addTask(6, new EntityAIWander(this, 3));
+			this.tasks.addTask(7, new EntityAIPanic(this, 1.2));
+		}
+
+		@Override
+		public EnumCreatureAttribute getCreatureAttribute() {
+			return EnumCreatureAttribute.UNDEFINED;
+		}
+
+		@Override
+		protected Item getDropItem() {
+			return null;
+		}
+
+		@Override
+		public net.minecraft.util.SoundEvent getAmbientSound() {
+			return (net.minecraft.util.SoundEvent) net.minecraft.util.SoundEvent.REGISTRY.getObject(new ResourceLocation(""));
+		}
+
+		@Override
+		public net.minecraft.util.SoundEvent getHurtSound(DamageSource ds) {
+			return (net.minecraft.util.SoundEvent) net.minecraft.util.SoundEvent.REGISTRY.getObject(new ResourceLocation("entity.generic.hurt"));
+		}
+
+		@Override
+		public net.minecraft.util.SoundEvent getDeathSound() {
+			return (net.minecraft.util.SoundEvent) net.minecraft.util.SoundEvent.REGISTRY.getObject(new ResourceLocation("entity.generic.death"));
+		}
+
+		@Override
+		protected float getSoundVolume() {
+			return 1.0F;
+		}
+
+		@Override
+		protected void applyEntityAttributes() {
+			super.applyEntityAttributes();
+			if (this.getEntityAttribute(SharedMonsterAttributes.ARMOR) != null)
+				this.getEntityAttribute(SharedMonsterAttributes.ARMOR).setBaseValue(0D);
+			if (this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED) != null)
+				this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.3D);
+			if (this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH) != null)
+				this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(18D);
+			if (this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE) != null)
+				this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(3D);
+		}
+	}
+}
